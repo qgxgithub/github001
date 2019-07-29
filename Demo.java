692d95c84e04d3477d3b12a3d8789df7c75b0c0e
qiguangxue <qgxlovebeyond@163.com>
2019-07-29 12:37:09 +0800

    this is a java

---
+++ Demo.java
@@ -0,0 +1,14 @@
+package com.bjsxt.scalaspark;
+
+/**
+ * TODO <br/>
+ * ClassName：Demo <br/>
+ * Author：QGX <br/>
+ * Date：2019/7/29 12:32 <br/>
+ * Version：1.0
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+}
