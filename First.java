a280f767efd72489c113291ac0ce961b7500ee2a
qiguangxue <qgxlovebeyond@163.com>
2019-07-29 13:12:45 +0800

    新增案例类  输出一行字符串

---
+++ First.java
@@ -0,0 +1,15 @@
+package com.bjsxt.scalaspark;
+
+/**
+ * TODO <br/>
+ * ClassName：Demo <br/>
+ * Author：QGX <br/>
+ * Date：2019/7/29 12:32 <br/>
+ * Version：1.0
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+        System.out.println("Hello Word Java");
+    }
+}
